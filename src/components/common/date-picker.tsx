608849b7fb221dc59197210cfca036004b1f315a
Otochicatole <164841448+Otochicatole@users.noreply.github.com>
2025-06-02 18:42:52 -0300

    message

--- src/components/common/date-picker.tsx
+++ src/components/common/date-picker.tsx
@@ -32,11 +32,12 @@ const CustomDatePicker: React.FC<CustomDatePickerProps> = ({
   const [lastName, setLastName] = useState<string>("");
   const [phoneNumber, setPhoneNumber] = useState<string>("");
   const [error, setError] = useState<string>("");
-  const [successMessage, setSuccessMessage] = useState<string>("");
+  const [successMessage, setSuccessMessage] = useState<string | React.JSX.Element>("");
+  const [showSuccessCard, setShowSuccessCard] = useState<boolean>(false);
 
   const handleDateChange = (date: Dayjs | null) => {
     setSelectedDate(date);
-    setSelectedTime(""); // Reset time when date changes
+    setSelectedTime("");
   };
 
   const handleTimeChange = (event: SelectChangeEvent<string>) => {
@@ -63,9 +64,11 @@ const CustomDatePicker: React.FC<CustomDatePickerProps> = ({
     if (!firstName || !lastName || !selectedDate || !selectedTime) {
       setError("Todos los campos son requeridos.");
       setSuccessMessage("");
+      setShowSuccessCard(false);
     } else {
       setError("");
       setSuccessMessage("");
+      setShowSuccessCard(false);
 
       const turno = {
         firstName,
@@ -91,9 +94,17 @@ const CustomDatePicker: React.FC<CustomDatePickerProps> = ({
           },
           body: JSON.stringify({ data: turno }),
         });
-
         if (res.ok) {
-          setSuccessMessage("El turno se ha guardado correctamente.");
+          const messageSuccess = <div className="">
+            <h2 className="text-2xl">El turno se ha guardado correctamente.</h2>
+            <p className=" text-orange-400/60">Tu turno se confirmarara via WhatsApp.</p>
+            <p className="text-sm text-red-500/60">Si tu turno no fue confirmado dentro de las 24 horas, por favor contactanos por WhatsApp.</p>
+            <p className="mt-4">Puedes revisar los turnos confirmados en la sección de &apos;Turnos Confirmados&apos;.</p>
+            <p className="mt-1">Si tienes alguna duda, no dudes en contactarnos.</p>
+            <p className="text-white/50 mt-10">¡Gracias por elegirnos!</p>
+          </div>
+          setSuccessMessage(messageSuccess);
+          setShowSuccessCard(true);
           setFirstName("");
           setLastName("");
           setPhoneNumber("");
@@ -126,74 +137,94 @@ const CustomDatePicker: React.FC<CustomDatePickerProps> = ({
 
   return (
     <div>
-      <TextField
-        label="Nombre"
-        value={firstName}
-        onChange={handleFirstNameChange}
-        fullWidth
-        margin="normal"
-        required
-      />
-      <TextField
-        label="Apellido"
-        value={lastName}
-        onChange={handleLastNameChange}
-        fullWidth
-        margin="normal"
-        required
-      />
-      <TextField
-        label="Número de teléfono"
-        value={phoneNumber}
-        onChange={handlePhoneNumberChange}
-        fullWidth
-        margin="normal"
-        required
-      />
-      <DatePicker
-        sx={{ width: "100%", marginTop: "16px" }}
-        label="Selecciona una fecha"
-        value={selectedDate}
-        onChange={handleDateChange}
-        shouldDisableDate={isDateUnavailable}
-        slots={{ textField: TextField }}
-      />
-      {selectedDate && (
-        <FormControl fullWidth margin="normal" required>
-          <InputLabel id="time-select-label">Selecciona un horario</InputLabel>
-          <Select
-            labelId="time-select-label"
-            value={selectedTime}
-            onChange={handleTimeChange}
-            label="Selecciona un horario"
-          >
-            {availableTimes.map((time) => (
-              <MenuItem key={time} value={time}>
-                {time}
-              </MenuItem>
-            ))}
-          </Select>
-        </FormControl>
-      )}
-      <Box mt={2}>
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={handleConfirm}
-          fullWidth
+      {showSuccessCard ? (
+        <Box
+          sx={{
+            borderRadius: "8px",
+            padding: "16px",
+            textAlign: "center",
+            backgroundColor: "rgba(76, 175, 80, 0.1)",
+          }}
         >
-          Confirmar Turno
-        </Button>
-      </Box>
-      {error && (
-        <Typography color="error" variant="body2" mt={2}>
-          {error}
-        </Typography>
-      )}
-      {successMessage && (
-        <Typography color="success" variant="body2" mt={2}>
-          {successMessage}
-        </Typography>
+          <Typography variant="h6" color="success">
+            {successMessage}
+          </Typography>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() => setShowSuccessCard(false)}
+            sx={{ marginTop: "16px" }}
+          >
+            Cerrar
+          </Button>
+        </Box>
+      ) : (
+        <>
+          <TextField
+            label="Nombre"
+            value={firstName}
+            onChange={handleFirstNameChange}
+            fullWidth
+            margin="normal"
+            required
+          />
+          <TextField
+            label="Apellido"
+            value={lastName}
+            onChange={handleLastNameChange}
+            fullWidth
+            margin="normal"
+            required
+          />
+          <TextField
+            label="Número de teléfono"
+            value={phoneNumber}
+            onChange={handlePhoneNumberChange}
+            fullWidth
+            margin="normal"
+            required
+          />
+          <DatePicker
+            sx={{ width: "100%", marginTop: "16px" }}
+            label="Selecciona una fecha"
+            value={selectedDate}
+            onChange={handleDateChange}
+            shouldDisableDate={isDateUnavailable}
+            slots={{ textField: TextField }}
+          />
+          {selectedDate && (
+            <FormControl fullWidth margin="normal" required>
+              <InputLabel id="time-select-label">Selecciona un horario</InputLabel>
+              <Select
+                labelId="time-select-label"
+                value={selectedTime}
+                onChange={handleTimeChange}
+                label="Selecciona un horario"
+              >
+                {availableTimes.map((time) => (
+                  <MenuItem key={time} value={time}>
+                    {time}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+          )}
+          <Box mt={2}>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={handleConfirm}
+              fullWidth
+            >
+              Confirmar Turno
+            </Button>
+          </Box>
+          {error && (
+            <Typography color="error" variant="body2" mt={2}>
+              {error}
+            </Typography>
+          )}
+        </>
       )}
     </div>
   );
